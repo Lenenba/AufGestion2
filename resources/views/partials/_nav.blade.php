a121e9db6efa85f8c561bf769556f0beef57253e
lenenba <bilitikjulesroger@yahoo.fr>
2020-09-25 20:08:50 -0400

    initialisation de la barre de naviguation

---
+++ resources/views/partials/_nav.blade.php
@@ -0,0 +1,46 @@
+<nav class="flex items-center justify-between">
+                <div class="flex items-end mb-2">
+                    <img src="/images/logo.png" alt="logo AUF" class="h-10 sm:ml-0 ">  
+                    
+                    <ul class=" flex text-xs lg:text-sm lg:ml-20 ml-6 text-gray-600 ">
+                        <li>
+                            <div class="flex mx-4 items-center hover:text-gray-400 focus:text-gray-100 ">    
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-4 w-4 text-gray-600 ">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 17V7m0 10a2 2 0 01-2 2H5a2 2 0 01-2-2V7a2 2 0 012-2h2a2 2 0 012 2m0 10a2 2 0 002 2h2a2 2 0 002-2M9 7a2 2 0 012-2h2a2 2 0 012 2m0 10V7m0 10a2 2 0 002 2h2a2 2 0 002-2V7a2 2 0 00-2-2h-2a2 2 0 00-2 2" />
+                                </svg>
+                                DASHBOARD
+                           </div>
+                        </li> <span class="text-gray-600"> | </span>
+                        <li>
+                            <div class="flex ml-4 items-center hover:text-gray-400 focus:text-gray-100 ">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-4 w-4 text-gray-600 ">
+                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
+                                </svg>
+                              SUPPLIER
+                            </div>    
+                        </li>
+                    </ul>
+                </div>  
+                <div class="flex items-center overflow-hidden">
+                    <div class="flex mr-5 ">
+                        <div class="relative">
+                            <span class="absolute rounded-full bg-red-700 text-white text-xs font-bold px-1 ml-5 ">2</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-5 mx-2 text-gray-600  hover:text-gray-400 focus:text-gray-100">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+                            </svg>
+                        </div>
+                        <div>
+                            <span class="absolute rounded-full bg-red-700 text-white text-xs font-bold px-1 ml-5 ">2</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-5 mx-2 text-gray-600  hover:text-gray-400 focus:text-gray-100">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z" />
+                            </svg>
+                        </div>
+                        
+                    </div> 
+                        <span class="text-gray-600"> | </span>
+                    <div class="flex items-center ml-4">
+                        <h6 class="text-gray-600 hover:text-gray-400 focus:text-gray-100 text-sm">Jules BILITIK</h6>
+                        <img src="https://fr.gravatar.com/avatar/md5("bilitikjulesroger@yahoo.fr")" alt="logo AUF" class="w-10 ml-3 rounded rounded-full shadow-solid">  
+                    </div>
+                </div>
+            </nav>
