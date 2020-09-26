350cb5bb498918f96c0ba152764d6c10cb4d2360
lenenba <bilitikjulesroger@yahoo.fr>
2020-09-25 22:21:10 -0400

    definition de petite card pour les données stat

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,11 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use SebastianBergmann\CodeCoverage\Report\Xml\Totals;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('dashboard.index');
+        $supplier =  $this->totalElements('fscmRestApi', 'suppliers');
+        $project =  $this->totalElements('fscmRestApi', 'projects');
+        $employer =  $this->totalElements('hcmRestApi', 'emps');
+        $compte =  $this->totalElements('crmRestApi', 'accounts');
+       
+       
+        return view('dashboard.index', compact('supplier', 'project', 'compte', 'employer'));
+    }
+
+
+    public function totalElements($modules, $elements)
+    {
+        $total = null;
+
+        $supplier =  Http::withBasicAuth('jules.bilitik@auf.org', 'Kiliane486')
+                            ->withHeaders([
+                                'content-type' =>'application/json',
+                                'REST-Framework-Version' => '8'])
+                            ->timeout(1200)
+                            ->get('https://ejxa.fa.ca2.oraclecloud.com:443/'.$modules.'/resources/11.13.18.05/'.$elements.'?onlyData=true&totalResults=true')
+                            ->json();
+
+        $total = $supplier['totalResults'];
+
+        return $total;
     }
 }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,6 +1,22 @@
 @extends('layouts.layout')
 @section('content')
 
-test body
+    <div class="flex items-center px-8 py-5">
+       @include('partials._smallCard', ['titre' => 'FOURNISSEURS', 
+                                        'elts' => $supplier, 
+                                        'color' => 'blue'])
+
+        @include('partials._smallCard', ['titre' => 'EMPLOYÉS', 
+                                        'elts' => $employer, 
+                                        'color' => 'indigo'])
+    
+        @include('partials._smallCard', ['titre' => 'PROJETS', 
+                                        'elts' => $project, 
+                                        'color' => 'green'])
+
+        @include('partials._smallCard', ['titre' => 'COMPTES', 
+                                        'elts' => $compte, 
+                                        'color' => 'orange'])
+    </div>
 
 @endsection
