1da2c7e921f1a0d3f183f08c37d68bbdb9571d08
Lenenba <bilitikjulesroger@yahoo.fr>
2020-11-17 15:51:03 -0500

    Update helpers.php

--- app/helpers.php
+++ app/helpers.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Http;
  function totalElts($modules, $elements, $append, $orderby)
  {
      $objects =  Http::withHeaders([
-                            'Authorization' => 'Basic anVsZXMuYmlsaXRpa0BhdWYub3JnOktpbGlhbmU0ODY=',
+                            'Authorization' => 'Basic ',
                             'content-type' =>'application/json'])
                         ->timeout(1200)
                         ->get('https://ejxa.fa.ca2.oraclecloud.com:443/'.$modules.'/resources/11.13.18.05/'.$elements.'?'.$append.'onlyData=true&expand=all&limit=5&totalResults=true&orderBy='.$orderby.':desc')
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Http;
   function listElts($liens)
   {
       $objects =  Http::withHeaders([
-            'Authorization' => 'Basic anVsZXMuYmlsaXRpa0BhdWYub3JnOktpbGlhbmU0ODY=',
+            'Authorization' => 'Basic',
             'content-type' =>'application/json'])
         ->timeout(1200)
         ->get($liens)
@@ -31,8 +31,7 @@ use Illuminate\Support\Facades\Http;
 
   function listElts2($liens)
   {
-      $objects =  Http::withHeaders([
-            'Authorization' => 'Basic anVsZXMuYmlsaXRpa0BhdWYub3JnOkBVRlByaW1hJjByYWNsZQ==',
+      $objects =  Http::withHeaders([',
             'content-type' =>'application/json'])
         ->timeout(1200)
         ->get($liens)
